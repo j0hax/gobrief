1e80f4daa089bb7863474e715d39aec8b8f80cc4
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2023-04-03 08:27:22 +0200

    Print Calendar with event

--- main.go
+++ main.go
@@ -2,39 +2,14 @@ package main
 
 import (
 	"log"
-	"sort"
-	"sync"
 
-	"github.com/apognu/gocal"
 	"github.com/j0hax/gobrief/config"
 )
 
 func main() {
 	cfg := config.LoadConfig()
-	events := make([]gocal.Event, 0, 64)
-	results := make(chan gocal.Event, len(cfg.Calendars))
 
-	// fetch each URL concurrently
-	var wg sync.WaitGroup
-	for _, url := range cfg.Calendars {
-		wg.Add(1)
-		go fetchCal(url, cfg.Days, results, &wg)
-	}
-
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-
-	// Add each item to the list:
-	// The range ends when all goroutines are finished and the channel is closed.
-	for r := range results {
-		events = append(events, r)
-	}
-
-	sort.Slice(events, func(i, j int) bool {
-		return events[i].Start.Before(*events[j].Start)
-	})
+	events := Fetch(cfg.Days, cfg.Calendars)
 
 	printCal(events)
 
